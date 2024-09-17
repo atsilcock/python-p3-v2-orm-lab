64fd9c362227cf588d1b043ee953d167f05d782c
Alin Silcock <at.silcock@gmail.com>
2024-09-16 22:00:21 -0600

    add

--- lib/employee.py
+++ lib/employee.py
@@ -42,7 +42,7 @@ class Employee:
             self._job_title = job_title
         else:
             raise ValueError(
-                "job_title must be a non-empty string"
+                "Job title must be a non-empty string"
             )
 
     @property
@@ -51,11 +51,12 @@ class Employee:
 
     @department_id.setter
     def department_id(self, department_id):
-        if type(department_id) is int and Department.find_by_id(department_id):
+        if isinstance(department_id, int) and Department.find_by_id(department_id):
             self._department_id = department_id
         else:
             raise ValueError(
-                "department_id must reference a department in the database")
+                "Department ID must reference a valid department in the database"
+            )
 
     @classmethod
     def create_table(cls):
@@ -81,12 +82,12 @@ class Employee:
         CONN.commit()
 
     def save(self):
-        """ Insert a new row with the name, job title, and department id values of the current Employee object.
-        Update object id attribute using the primary key value of new row.
+        """ Insert a new row with the name, job title, and department ID values of the current Employee object.
+        Update object ID attribute using the primary key value of new row.
         Save the object in local dictionary using table row's PK as dictionary key"""
         sql = """
-                INSERT INTO employees (name, job_title, department_id)
-                VALUES (?, ?, ?)
+            INSERT INTO employees (name, job_title, department_id)
+            VALUES (?, ?, ?)
         """
 
         CURSOR.execute(sql, (self.name, self.job_title, self.department_id))
@@ -108,7 +109,7 @@ class Employee:
 
     def delete(self):
         """Delete the table row corresponding to the current Employee instance,
-        delete the dictionary entry, and reassign id attribute"""
+        delete the dictionary entry, and reassign ID attribute"""
 
         sql = """
             DELETE FROM employees
@@ -118,10 +119,10 @@ class Employee:
         CURSOR.execute(sql, (self.id,))
         CONN.commit()
 
-        # Delete the dictionary entry using id as the key
+        # Delete the dictionary entry using ID as the key
         del type(self).all[self.id]
 
-        # Set the id to None
+        # Set the ID to None
         self.id = None
 
     @classmethod
@@ -135,15 +136,15 @@ class Employee:
     def instance_from_db(cls, row):
         """Return an Employee object having the attribute values from the table row."""
 
-        # Check the dictionary for  existing instance using the row's primary key
+        # Check the dictionary for an existing instance using the row's primary key
         employee = cls.all.get(row[0])
         if employee:
-            # ensure attributes match row values in case local instance was modified
+            # Ensure attributes match row values in case local instance was modified
             employee.name = row[1]
             employee.job_title = row[2]
             employee.department_id = row[3]
         else:
-            # not in dictionary, create new instance and add to dictionary
+            # Not in dictionary, create new instance and add to dictionary
             employee = cls(row[1], row[2], row[3])
             employee.id = row[0]
             cls.all[employee.id] = employee
@@ -179,7 +180,7 @@ class Employee:
         sql = """
             SELECT *
             FROM employees
-            WHERE name is ?
+            WHERE name = ?
         """
 
         row = CURSOR.execute(sql, (name,)).fetchone()
@@ -187,4 +188,14 @@ class Employee:
 
     def reviews(self):
         """Return list of reviews associated with current employee"""
-        pass
+        from review import Review
+        sql = """
+            SELECT * FROM reviews
+            WHERE employee_id = ?
+        """
+        CURSOR.execute(sql, (self.id,),)
+
+        rows = CURSOR.fetchall()
+        return [
+            Review.instance_from_db(row) for row in rows
+        ]
